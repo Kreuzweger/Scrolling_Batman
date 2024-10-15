9cedf6a035a3dd0ada9587fab2263393c1ac67c8
Andreas-pixel <andreas.grasboeck@htl1.at>
2024-10-15 10:11:42 +0200

    ButtonScroll

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-let lastScrollTop = 0;
-let rotation = 0;
 
-window.addEventListener("scroll", function () {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  
-  // Berechne die Scrollgeschwindigkeit
-  let scrollSpeed = scrollTop - lastScrollTop;
-  
-  // Aktualisiere die Rotation basierend auf der Scrollgeschwindigkeit
-  rotation += scrollSpeed * 0.5; // Passe diesen Wert für die Empfindlichkeit an
+gsap.registerPlugin(ScrollTrigger);
 
-  // Wende die Rotation auf das Logo an
-  document.querySelector(".logo").style.transform = `rotate(${rotation}deg)`;
-
-  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Damit es nicht negativ wird
+gsap.to(".logo", {
+  scrub:4,
+  rotation: 720,  
+  ease: "none",   
+  scrollTrigger: {
+    trigger: ".spacer",   
+    start: "top top",    
+    end: "bottom bottom", 
+    scrub: true,          
+    markers: false,
+    pin: true        
+  }
 });
