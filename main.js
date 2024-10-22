ef540dbb5d6323db5a5350d972fca4ddded1466f
Andreas-pixel <andreas.grasboeck@htl1.at>
2024-10-22 10:22:03 +0200

    Bats und Schrift

--- main.js
+++ main.js
@@ -14,3 +14,58 @@ gsap.to(".logo", {
     pin: true        
   }
 });
+
+// gsap.fromTo(".Bats img", 
+//   {
+//     x: '-100vw',  
+//     y: '-50vh',  
+//     opacity: 0   
+//   },
+//   {
+//     scrub: 4,
+//     x: '20vw',  
+//     y: '40vh',
+//     opacity: 1,  
+//     duration: 500, 
+//     scrollTrigger: {
+//       trigger: ".Bats",
+//       start: "top", 
+//       end: "+600", 
+//       scrub: true, 
+//       markers: true, 
+//     },
+//     onComplete: () => { 
+//       gsap.to(".Bats img", {
+//         opacity: 0,
+//         duration: 1,  
+//       });
+//     }
+//   }
+// );
+
+gsap.fromTo(".bats img", 
+  {
+    x: '-100vw', 
+    y: '-10vh',  
+    opacity: 0   
+  },
+  {
+    x: '20vw',    
+    y: '20vh',    
+    opacity: 1,  
+    duration: 3,  
+    scrollTrigger: {
+      trigger: ".bats",
+      start: "30% top", 
+      end: "+=500",      
+      scrub: true,      
+      markers: false,   
+    },
+    onComplete: () => {  
+      gsap.to(".bats img", {
+        opacity: 0,
+        duration: 1,    
+      });
+    }
+  }
+);
