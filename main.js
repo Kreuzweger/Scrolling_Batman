3b48837a18bdb2a2161db5b11168142e102c6fbd
Andreas-pixel <andreas.grasboeck@htl1.at>
2024-11-05 10:20:23 +0100

    platzierung

--- main.js
+++ main.js
@@ -15,57 +15,46 @@ gsap.to(".logo", {
   }
 });
 
-// gsap.fromTo(".Bats img", 
+
+// gsap.fromTo(".bats img", 
 //   {
-//     x: '-100vw',  
-//     y: '-50vh',  
+//     x: '-100vw', 
+//     y: '-10vh',  
 //     opacity: 0   
 //   },
 //   {
-//     scrub: 4,
-//     x: '20vw',  
-//     y: '40vh',
+//     x: '20vw',    
+//     y: '20vh',    
 //     opacity: 1,  
-//     duration: 500, 
+//     duration: 3,  
 //     scrollTrigger: {
-//       trigger: ".Bats",
-//       start: "top", 
-//       end: "+600", 
-//       scrub: true, 
-//       markers: true, 
+//       trigger: ".bats",
+//       start: "30% top", 
+//       end: "+=500",      
+//       scrub: true,      
+//       markers: false,   
 //     },
-//     onComplete: () => { 
-//       gsap.to(".Bats img", {
+//     onComplete: () => {  
+//       gsap.to(".bats img", {
 //         opacity: 0,
-//         duration: 1,  
+//         duration: 1,    
 //       });
 //     }
 //   }
 // );
 
-gsap.fromTo(".bats img", 
-  {
-    x: '-100vw', 
-    y: '-10vh',  
-    opacity: 0   
-  },
-  {
-    x: '20vw',    
-    y: '20vh',    
-    opacity: 1,  
-    duration: 3,  
-    scrollTrigger: {
-      trigger: ".bats",
-      start: "30% top", 
-      end: "+=500",      
-      scrub: true,      
-      markers: false,   
-    },
-    onComplete: () => {  
-      gsap.to(".bats img", {
-        opacity: 0,
-        duration: 1,    
-      });
-    }
-  }
-);
+window.onload = function() {
+  const blackScreen = document.getElementById('black-screen');
+  const content = document.getElementById('content');
+
+  // Warten Sie 2 Sekunden, bevor Sie den Fade-Out des schwarzen Bildschirms starten
+  setTimeout(() => {
+      blackScreen.style.opacity = '0'; // Fade-Out des schwarzen Bildschirms
+
+      // Warten, bis die Fade-Out Animation abgeschlossen ist
+      setTimeout(() => {
+          blackScreen.style.display = 'none'; // Entfernen Sie den schwarzen Bildschirm
+          content.classList.add('show'); // Fügen Sie die Klasse zum Einblenden des Inhalts hinzu
+      }, 1000); // entspricht der Dauer der Fade-Out-Animation
+  }, 500); // 2 Sekunden warten, bevor der Fade-Out beginnt
+};
