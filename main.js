8d000bebd5a4074af56bac8eae56edada9923a7f
Andreas-pixel <andreas.grasboeck@htl1.at>
2025-01-14 10:16:54 +0100

    final cchanges

--- main.js
+++ main.js
@@ -109,13 +109,13 @@ gsap.registerPlugin(ScrollTrigger);
 
 // Horizontal scroll effect for batcave
 gsap.to(".background", {
-  x: "-68%", // Move the background fully horizontally
+  x: "-67%", // Move the background fully horizontally
   ease: "power1.inOut", // Smooth easing
   scrollTrigger: {
     trigger: ".scroll-section",  // Start based on the scroll section
     start: "top top",            // Start when the section hits the top
     end: "bottom center",        // End scroll halfway through
-    scrub: 5,                    // Synchronize with scroll
+    scrub: 8,                    // Synchronize with scroll
     pin: true,                   // Pin the section during the scroll
     pinSpacing: false,           // Prevent extra spacing
     onUpdate: (self) => {
